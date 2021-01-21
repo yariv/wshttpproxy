6a41b1b448a5dba47e2fb9b539554cfc357bc05f
Yariv Sadan <yarivsadan@gmail.com>
2021-01-21 15:33:10 -0800

    update

--- router/src/typedApi/apiSchema.ts
+++ router/src/typedApi/apiSchema.ts
@@ -1,14 +1,5 @@
 import * as z from "zod";
 
-export type SchemaType = typeof apiSchema;
-export type MethodType = keyof SchemaType;
-export type ReqSchema<MethodName extends MethodType> = z.infer<
-  typeof apiSchema[MethodName]["reqSchema"]
->;
-export type ResSchema<MethodName extends MethodType> = z.infer<
-  typeof apiSchema[MethodName]["resSchema"]
->;
-
 export const apiSchema = {
   createApplication: {
     reqSchema: z.object({
