8a4b6c71b1b2accbea3c0ce4b2be46c5fe1298af
Yariv Sadan <yarivsadan@gmail.com>
2021-02-24 14:05:21 -0800

    update

--- dbproxy/src/main.ts
+++ dbproxy/src/main.ts
@@ -1,5 +1,6 @@
 import net from "net";
 import mysql from "mysql2";
+import mysql2 from "mysql2/promise";
 import Connection from "mysql2/typings/mysql/lib/Connection";
 import util from "util";
 import { Parser } from "node-sql-parser";
@@ -30,21 +31,22 @@ const COMPRESS = 0x00000020; // from  require("mysql2/lib/constants/client");
 const port = 1235;
 
 const parser = new Parser();
-const getClientConnection = async (): Promise<Connection> => {
-  var clientConn = mysql.createConnection({
+const getClientConnection = async (): Promise<mysql2.Connection> => {
+  var clientConn = mysql2.createConnection({
     host: "127.0.0.1",
     user: "root",
     password: "root",
     database: "test",
     port: 3306,
   });
-
-  const connect = util.promisify(clientConn.connect.bind(clientConn));
-  await connect();
   return clientConn;
+
+  // const connect = util.promisify(clientConn.connect.bind(clientConn));
+  // await connect();
+  // return clientConn;
 };
 
-let proxyConn: Connection;
+let proxyConn: mysql2.Connection;
 const listen = async (port: number): Promise<() => Promise<void>> => {
   const server = (mysql as any).createServer();
   server.on("connection", async (conn: Connection) => {
@@ -62,19 +64,21 @@ const listen = async (port: number): Promise<() => Promise<void>> => {
     });
 
     console.log("connected");
-    conn.on("query", (query: any) => {
+    conn.on("query", async (query: any) => {
       console.log("got query", query);
       //console.log(parser.astify(query));
-      proxyConn.query(query, (err, results, fields) => {
-        // TODO
-        if (err) throw err;
-        console.log("result", err, results, fields);
-        if (Array.isArray(results)) {
-          (conn as any).writeTextResult(results, fields);
-        } else {
-          (conn as any).writeOk(results);
-        }
-      });
+      const [results, fields] = await proxyConn.query(query);
+      console.log("res", results, fields);
+      // proxyConn.query(query, (err, results, fields) => {
+      //   // TODO
+      //   if (err) throw err;
+      //   console.log("result", err, results, fields);
+      //   if (Array.isArray(results)) {
+      //     (conn as any).writeTextResult(results, fields);
+      //   } else {
+      //     (conn as any).writeOk(results);
+      //   }
+      // });
     });
     conn.on("error", (error: any) => {});
   });
