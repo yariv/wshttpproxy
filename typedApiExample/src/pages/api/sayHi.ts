d4166944503f1c1be5f029bb0446abb6eafae4e9
Yariv Sadan <yarivsadan@gmail.com>
2021-03-11 12:24:14 -0800

    update

--- typedApiExample/src/pages/api/sayHi.ts
+++
@@ -1,6 +0,0 @@
-import { createNextHandler } from "../../../../src/nextAdapter";
-import { schema } from "../../schema";
-
-export default createNextHandler(schema, "sayHi", async ({ name }) => {
-  return "Hi " + name;
-});
